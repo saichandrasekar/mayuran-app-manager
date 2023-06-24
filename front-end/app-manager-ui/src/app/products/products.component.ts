14688574ec5dc0d586e0cf94301e2857ed23f9f9
saichandrasekar <saichandrasekar@gmail.com>
2023-06-24 11:57:43 +0530

    added pagination, sort

--- front-end/app-manager-ui/src/app/products/products.component.ts
+++ front-end/app-manager-ui/src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {DataSource} from '@angular/cdk/collections';
 import {Observable, ReplaySubject} from 'rxjs';
 import {MatButtonModule} from '@angular/material/button';
@@ -9,36 +9,47 @@ import { Product } from '../models/product.model';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {SelectionModel} from '@angular/cdk/collections';
 
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
   standalone: true,
-  imports: [MatButtonModule, MatTableModule, MatCheckboxModule]
+  imports: [MatButtonModule, MatCheckboxModule,MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
 })
-export class ProductsComponent {
-
-constructor(private productsService: ProductsServiceService, private router: Router){
-  //this.dataToDisplay = this.productsService.getProducts();
-  //this.dataSource = new MatTableDataSource<Product>([]);
-}
-
-
-  displayedColumns: string[] = ['select','name','applicationCategory','manufacturer','businessProcess','applicationType','applicationFamily','architectureType','installType','technolocyStack','userBase','platform','contractEndDate','supportVendor','businessCriticality','dataClassification','currency','rights'];
-  //dataToDisplay: Product[] = [];
-
-  //dataSource = new ExampleDataSource(this.dataToDisplay);
-  dataToDisplay = this.productsService.getProducts();
-  dataSource = new MatTableDataSource<Product>(this.dataToDisplay);
-
+export class ProductsComponent implements AfterViewInit {
+  displayedColumns: string[] = ['select','name','applicationCategory','manufacturer','businessProcess','applicationType','applicationFamily','architectureType','installType','technolocyStack','userBase','platform','contractEndDate','supportVendor','businessCriticality','dataClassification','currency','rights']; 
+  dataSource : MatTableDataSource<Product>;
   selection = new SelectionModel<Product>(true, []);
-  addData() {
-    this.router.navigateByUrl('/products');
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;  
+
+  constructor(private productsService: ProductsServiceService, private router: Router){
+    let dataToDisplay = this.productsService.getProducts();
+    this.dataSource = new MatTableDataSource<Product>(dataToDisplay);
   }
 
-  removeData() {
-    this.dataToDisplay = this.dataToDisplay.slice(0, -1);
-    //this.dataSource.setData(this.dataToDisplay);
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  addData() {
+    this.router.navigateByUrl('/products');
   }
 
   isAllSelected() {
@@ -53,7 +64,6 @@ constructor(private productsService: ProductsServiceService, private router: Rou
       this.selection.clear();
       return;
     }
-
     this.selection.select(...this.dataSource.data);
   }
 
@@ -84,15 +94,3 @@ class ExampleDataSource extends DataSource<Product> {
     this._dataStream.next(data);
   }
 }
-
-// /**
-//  * @title Basic toolbar
-//  */
-// @Component({
-//   selector: 'toolbar-basic-example',
-//   templateUrl: 'toolbar-basic-example.html',
-//   styleUrls: ['toolbar-basic-example.css'],
-//   standalone: true,
-//   imports: [MatToolbarModule, MatButtonModule, MatIconModule],
-// })
-// export class ToolbarBasicExample {}
